3a942d2a0fc3453844db6c1194c57f3fc72cbd43
Ismail Sunni <imajimatika@gmail.com>
2019-11-21 17:01:15 +0100

    Add docker_image mapping.

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,27 @@ supported_Rs <- function(){
     )
   )
 }
+
+#' Get docker image for a combination of a platform and a R implementation.
+#'
+#' Return the name of docker image. Return empty string if the combination is not supported.
+#' @import dplyr
+#' @param platform The platform name see \link{supported_platforms}
+#' @param r_implementation The R implementation name. See \link{supported_Rs}
+#' @export
+#' @examples
+#' library("altRnative")
+#' docker_image("debian", "gnu-r")
+#' docker_image("ubuntu", "mro")
+#' docker_image("debian", "renjin")
+docker_image <- function(platform, r_implementation){
+  table <- tibble(
+    dist = c("debian", "ubuntu"),
+    R = c("gnu-r", "mro"),
+    image_name = c("ismailsunni/gnur-3.6.1-debian-geospatial", "ismailsunni/mro-3.5.3-ubuntu-geospatial"))
+  result <- dplyr::filter(table, dist == platform, R == r_implementation)
+  return( pull(result, "image_name"))
+}
+
+
+
