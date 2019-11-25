54d9fcc97a6a80a42c803774da6dc772476bd1ff
Ismail Sunni <imajimatika@gmail.com>
2019-11-25 11:10:24 +0100

    Set default times parameter.

--- R/benchmark.R
+++ R/benchmark.R
@@ -5,26 +5,27 @@
 #' @param code An expression or string of R code
 #' @param platforms List of platform
 #' @param r_implementations List of R implementation
+#' @param times How many times the code will be run. Passed to \link{microbenchmark}
 #' @param ... Parameters for \link{microbenchmark}
 #' @export
 #' @examples
 #' benchmarks_code(code = "1 + 1", times = 3)
-benchmarks_code <- function(code, platforms = c("debian", "ubuntu"), r_implementations = c("gnu-r", "mro"), ...){
+benchmarks_code <- function(code, platforms = c("debian", "ubuntu"), r_implementations = c("gnu-r", "mro"), times = 3, ...){
   print(code)
   print(r_implementations)
   print(platforms)
-  expressions = c()
+  expressions = list()
   for (r_implementation in r_implementations){
     for(platform in platforms){
       if (length(docker_image(platform, r_implementation)) > 0){
         e = call("run_code", code, platform, r_implementation)
-        expressions <- append(expressions, e)
+        expressions[[paste(r_implementation, "on", platform)]] <- e
       } else {
         print(paste('Docker image for', r_implementation, "and", platform, "is not supported"))
       }
     }
   }
-  microbenchmark::microbenchmark(list = expressions, ...)
+  microbenchmark::microbenchmark(list = expressions, times = times, ...)
 }
 
 
@@ -35,12 +36,13 @@ benchmarks_code <- function(code, platforms = c("debian", "ubuntu"), r_implement
 #' @param r_file A file of R code
 #' @param r_implementations List of R implementation
 #' @param platforms List of platform
+#' @param times How many times the code will be run. Passed to \link{microbenchmark}
 #' @param ... Parameters for \link{microbenchmark}
 #' @export
 #' @examples
 #' file_path <- system.file('extdata/test.R', package = 'altRnative')
 #' benchmarks_file(file_path, times = 3)
-benchmarks_file <- function(r_file, platforms = c("debian", "ubuntu"), r_implementations = c("gnu-r", "mro"), ...){
+benchmarks_file <- function(r_file, platforms = c("debian", "ubuntu"), r_implementations = c("gnu-r", "mro"), times = 3, ...){
   print(r_file)
   print(r_implementations)
   print(platforms)
@@ -49,11 +51,11 @@ benchmarks_file <- function(r_file, platforms = c("debian", "ubuntu"), r_impleme
     for(platform in platforms){
       if (length(docker_image(platform, r_implementation)) > 0){
         e = call("run_file", r_file, platform, r_implementation)
-        expressions <- append(expressions, e)
+        expressions[[paste(r_implementation, "on", platform)]] <- e
       } else {
         print(paste('Docker image for', r_implementation, "and", platform, "is not supported"))
       }
     }
   }
-  microbenchmark::microbenchmark(list = expressions, ...)
+  microbenchmark::microbenchmark(list = expressions, times = times, ...)
 }
