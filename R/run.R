e941e50e56ddf5c72c5f7b2437d56803b3c62fa1
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-01-13 12:33:08 +0100

    pass ellipsis to stevedore

--- R/run.R
+++ R/run.R
@@ -5,6 +5,7 @@
 #' @param code An expression or string of R code
 #' @param docker_image A docker image name
 #' @param volumes Volume mapping from host to container
+#' @param ... Parameters passed on to \link{stevedore} client creation and container running functions
 #' @export
 #' @examples
 #' \dontrun{
@@ -36,9 +37,9 @@
 #' docker_run_code(code, "ismailsunni/gnur-3.6.1-debian-geospatial",
 #'                 volumes = '/home/ismailsunni/dev/r/sdsr:/home/docker/sdsr')
 #' }
-docker_run_code <- function(code, docker_image, volumes = NULL){
+docker_run_code <- function(code, docker_image, volumes = NULL, ...){
   # Prepare docker container
-  docker <- stevedore::docker_client()
+  docker <- stevedore::docker_client(...)
 
   # Prepare the script
   if (is.expression(code)){
@@ -51,7 +52,11 @@ docker_run_code <- function(code, docker_image, volumes = NULL){
   print(c("Rscript", "-e", full_code))
 
   # Run using stevedore
-  result <- docker$container$run(docker_image, c("Rscript", "-e", full_code), rm = TRUE, volumes = volumes)
+  result <- docker$container$run(docker_image,
+                                 c("Rscript", "-e", full_code),
+                                 rm = TRUE,
+                                 volumes = volumes,
+                                 ...)
 
   return(result)
 }
@@ -63,6 +68,7 @@ docker_run_code <- function(code, docker_image, volumes = NULL){
 #' @param r_file A file of R code
 #' @param docker_image A docker image name
 #' @param volumes Volume mapping from host to container
+#' @param ... Parameters passed on to \link{stevedore} client creation and container running functions
 #' @export
 #' @examples
 #' \dontrun{
@@ -71,14 +77,18 @@ docker_run_code <- function(code, docker_image, volumes = NULL){
 #' }
 docker_run_file <- function(r_file, docker_image, volumes = NULL, ...){
   # Prepare docker container
-  docker <- stevedore::docker_client()
+  docker <- stevedore::docker_client(...)
 
   # Debug purpose
   print("Full command")
   print(c("Rscript", r_file))
 
   # Create container
-  container <- docker$container$create(docker_image, c("R", "--no-save"), tty = TRUE, volumes = volumes)
+  container <- docker$container$create(docker_image,
+                                       c("R", "--no-save"),
+                                       tty = TRUE,
+                                       volumes = volumes,
+                                       ...)
   container$start()
   # Get working directory
   work_dir <- container$exec("pwd")$output
@@ -103,6 +113,8 @@ docker_run_file <- function(r_file, docker_image, volumes = NULL, ...){
 #' @param r_file A file of R code
 #' @param platform The platform name see \link{supported_platforms}
 #' @param r_implementation The R implementation name. See \link{supported_Rs}
+#' @param volumes Volume mapping from host to container
+#' @param ... Parameters passed on to \link{stevedore} client creation and container running functions
 #' @export
 #' @examples
 #' \dontrun{
@@ -110,9 +122,10 @@ docker_run_file <- function(r_file, docker_image, volumes = NULL, ...){
 #' run_file(file_path, 'debian', 'gnu-r')
 #' run_file(file_path, 'not-debian', 'gnu-r')
 #' }
+run_file <- function(r_file, platform = "debian", r_implementation = "gnu-r", volumes = NULL, ...){
   image_name <- docker_image(platform, r_implementation)
   if (length(image_name) > 0){
-    return(docker_run_file(r_file, image_name, volumes = NULL))
+    return(docker_run_file(r_file, image_name, volumes = NULL, ... = ...))
   } else {
     print(paste("No Docker Image for", platform, "and", r_implementation))
   }
@@ -124,14 +137,16 @@ docker_run_file <- function(r_file, docker_image, volumes = NULL, ...){
 #' @param platform The platform name see \link{supported_platforms}
 #' @param r_implementation The R implementation name. See \link{supported_Rs}
 #' @param volumes Volume mapping from host to container
+#' @param ... Parameters passed on to \link{stevedore} client creation and container running functions
 #' @export
 #' @examples
 #' \dontrun{
 #' run_code("a = 1 + 1", 'debian', 'gnu-r')
 #' }
+run_code <- function(code, platform = "debian", r_implementation = "gnu-r", volumes = NULL, ...){
   image_name <- docker_image(platform, r_implementation)
   if (length(image_name) > 0){
-    return(docker_run_code(code, image_name, volumes = volumes))
+    return(docker_run_code(code, image_name, volumes = volumes, ... = ...))
   } else {
     print(paste("No Docker image for", platform, "and", r_implementation))
   }
